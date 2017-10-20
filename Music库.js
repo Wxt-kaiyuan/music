cd1c41bb5dc891de56cb968cc3c5e98fbfa5e152
Wxt-kaiyuan <929209693@qq.com>
2017-10-20 16:09:48 +0800

    tow

---
+++ Music库.js
@@ -0,0 +1,175 @@
+/**
+ * Created by Administrator on 2017/10/20 0020.
+ */
+var data=[
+    {
+        url:'http://dl.stream.qqmusic.qq.com/C400000OIGu03YtdZj.m4a?vkey=52F28C641A1685F7882BBB71C2C57C2F557E9AB3CB6012EED40DDFFEBC0A0E3FDAF31AE6886FA36DA4B6890713936EC3038817B391E3450F&guid=0&uin=0&fromtag=66',
+        singer:'金志文',
+        song:'太傻',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004FPDFu4H5SLQ.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000qpN9133sGuQ.m4a?vkey=FBF7FA53CCF80578C895711EE3724E5423DE751B6FBC1702B74C0B50FB02847FBEF9AC18A519DCAE00F18D1B7AB2CFD3E32785985117EFAC&guid=0&uin=0&fromtag=66',
+        singer:'赵雷',
+        song:'画',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001sKd2l0dVkXa.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000MCRpx0myGAL.m4a?vkey=E8E9C1F46CF35E952F124020A66FCCDFF7E1E6193DFDECD4ECDB361CD5F57B42D5C9817676444C91AC24935854C2AB4F0A1EE2F4837A647E&guid=0&uin=0&fromtag=66',
+        singer:'赵雷',
+        song:'成都',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M00000490g2Q44YI6x.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004UOmIT0FnVzP.m4a?vkey=8558C1501198F148142D2EB629E8E5A0E38A9EAA47667874470C8B35D1433B6ECA792518AA7D1E5CE8E5C87BDAF1D14A2A3D905CBB185771&guid=0&uin=0&fromtag=66',
+        singer:'达布希勒图',
+        song:'第三人称',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004CwWbI4Anyus.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C4000045ZdLx1swQDu.m4a?vkey=B33C1C31DEC2B9D9310A2C54DBE3889D3874CB9118457B69A8EC89B782F87FF200A651919BE51DA722C3D363632A017D0FA7AC5637560529&guid=0&uin=0&fromtag=66',
+        singer:'达布希勒图',
+        song:'达尔文',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004CwWbI4Anyus.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400001CkBs10UxKxc.m4a?vkey=113B8F402D36E1B9F7AC070438C472888CB4E16576FCE587A6D7EF986249906482934ED45DFA5C3167707B284C530B10AE79DE0E8C8CCDDB&guid=0&uin=0&fromtag=66',
+        singer:'张碧晨&杨宗纬',
+        song:'凉凉',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M0000011IIJE3XYf9L.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000hcT5L3qazwZ.m4a?vkey=EF1821325B6045093E83EB32809BF99441F988E69138095C68F4C5D7D6BE1B6919FF834B9ABA58A3806E4A0F880E9AFC666EA98910D04BF9&guid=0&uin=0&fromtag=66',
+        singer:'薛之谦',
+        song:'遗憾',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002W7K2D0LxJJR.jpg?max_age=2592000'
+
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400002Eg7sW1b7tAr.m4a?vkey=D0A3FA46E6E2828A3EEC7E0E813C9917A5C222E00E0D23522ABBA14503391DBC8F517A4361AE3B631C06190EA32273378B5BCF836F260DE0&guid=0&uin=0&fromtag=66',
+        singer:'刘维/薛之谦',
+        song:'婚礼的祝福',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002W7K2D0LxJJR.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000e99cS3EF0iD.m4a?vkey=CADA09C8533E03C3FAA452C1BF889726234F1997E646A2587DB97A0B325171D662A55B07A8BADE1D18A0E76A85507B5BB478657E08BB1C3E&guid=0&uin=0&fromtag=66',
+        singer:'金志文',
+        song:'永不消逝',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002Du0Zw2MqqFb.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000hzQ2r0DLuFZ.m4a?vkey=2C3B8E5AF484D9775679F428AF6C63E4C236DAA9B65B1F7C59834897AC76BBED79A6C82C3BE66049CF12EAC0E51EF31E12FEC2BA8A6EF855&guid=0&uin=0&fromtag=66',
+        singer:'华晨宇',
+        song:'阿里山的姑娘',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001GtAkG2LByjR.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400003aUP2G2zsxDc.m4a?vkey=4E1A87B69655EF9649C858F976E5185135113F5718E41ED2E46359B2AF4E111F1238C60B04C5AC41B5DC51701982C0EDCE41CFBEDF50C330&guid=0&uin=0&fromtag=66',
+        singer:'杨坤',
+        song:'孤独颂',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001U8jxc0AzKF2.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000IRXra43Gb55.m4a?vkey=86C85C284322558ED524AB6A03D3A32280064A362528D9831161A326AB1218B28BDDD23C2C929018023CCC232386B0782F071DD3D16A74B2&guid=0&uin=0&fromtag=66',
+        singer:'狮子合唱团',
+        song:'天亮以后说分手',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M0000015FKwy4Enx52.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000zQWcd2hOgbg.m4a?vkey=91EBFE1024B40D5FA2C2E331E6E3E95E9A92AD5F0529ED8F9AC7B8DCC84130762D7E755323BCC6688BC85585CE9AD935DD51C41452A54763&guid=0&uin=0&fromtag=66',
+        singer:'张杰',
+        song:'回娘家',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001GtAkG2LByjR.jpg?max_age=2592000'
+
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000Rsy8H16Xvz8.m4a?vkey=B0BA049B44FCAC08690B2CD87CF454CDF03D7949031F494344835FD9D49190734D36D2EEB4CE36A8F5295435F8F93BF25B30DD93B88896EB&guid=0&uin=0&fromtag=66',
+        singer:'杨青柠/谭冰尧',
+        song:'小太阳',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004czy7T4BLvLc.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400001yS0N33yPm1B.m4a?vkey=A4CAC12C37F70BC97117989934EB132ACD082501DBDE9668B3EA548E230D080C0456E6A9527E5EC0CF4E8BE5797DEA4F202D7E4C12444EB4&guid=0&uin=0&fromtag=66',
+        singer:'beyond',
+        song:'海阔天空',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002qcJuX3lO3EZ.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400002pKRoX4Qbafa.m4a?vkey=CF140D7BFF9BCABF4E83111B9E8B9E51E276652D7FE5C3B6D06704152E4381A89E93EFBB4CCDAB41B7E7855E05861478EFDEFA083F1682CF&guid=0&uin=0&fromtag=66',
+        singer:'beyond',
+        song:'光辉岁月',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001C2BRX15iE4B.jpg?max_age=2592000'
+    }, {
+        url: 'http://dl.stream.qqmusic.qq.com/C4000018mMEo0QIheP.m4a?vkey=6476F8BCF85636B40F09851299825BF9460C6D444354B40AD166C491042606CE7B374537FFB401B92FAEF7ECF7D1C19DEB567E374B127628&guid=0&uin=0&fromtag=66',
+        singer: 'beyond',
+        song: '冷雨夜',
+        pic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002MGTsZ3Zl6JH.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004Gyue33FERTT.m4a?vkey=C1C39DEA5E8B40E2A203DA2CD7BC0C29391BE5124951F1843D7667B513FE658F1F12FBD9C0D6152F04A8A0796E2A699A9BCA9977E6A08115&guid=0&uin=0&fromtag=66',
+        singer:'beyond',
+        song:'真的爱你',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000000eOgmK1fN8Cs.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400002jTEqy2qtPRB.m4a?vkey=AC0752C994E98C3A6311245CABD360FCF4F67242AE6DBD9C27C32BF8CCBC9D852DEDE8176EC1FD99A720785D0BFF9F0D2E2D4F8242C38FE3&guid=0&uin=0&fromtag=66',
+        singer:'beyond',
+        song:'不再犹豫',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002XSb5j2bTyEh.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C4000027zPYs3A9fyb.m4a?vkey=77B9D4C12A0D05208DCEDF7B0BF31A7AA339041704544FB789475A9BB99E4B0AB90F82826F7A19A1F382E25B53CF954EF54F94220E3E8BC8&guid=0&uin=0&fromtag=66',
+        singer:'金志文',
+        song:'远走高飞',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004U936F0HXo1B.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400003OUlho2HcRHC.m4a?vkey=B72BB362F81B79FA3E106790348A1B029F49EB423144A15508671DC4CAE8C0D932693C704E1C743732EFB6F2CCEA9E3C76C49CED245C6053&guid=0&uin=0&fromtag=66',
+        singer:'周杰伦',
+        song:'告白气球',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000003RMaRI1iFoYd.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004PPMfE3BpMZG.m4a?vkey=C9B8CA9539505BF5F167995587FE8C3D83BBA939FF72FABF5421B76F18CDF41C2D21E7D6CBCE18F2F551E412B9666D980D2BA559A75E5A2D&guid=0&uin=0&fromtag=66',
+        singer:'nana',
+        song:'Lonely',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000000V1USK3p6z4D.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C4000006N7ge388PxV.m4a?vkey=CA1908A609EAA8650F119E536C22F0939CDDBDD0F8708212A4B879EA8A1255842B95E7D7BDDEA2D35FB1B9854519F0F3C4230DE039DE347D&guid=0&uin=0&fromtag=66',
+        singer:'Wiz Khalifa / Charlie Puth',
+        song:'See You Again',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M0000001QUHr0ZY6kM.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400003AcnTB2srtFk.m4a?vkey=9C85F965723C5881A25E1E9B1173CC73EC8DDE9CE71F96F90F14AE54283B5322B6FD3E70283BF8B0B48730A1642668D5D6060076A1BA897C&guid=0&uin=0&fromtag=66',
+        singer:'The Downtown Fiction',
+        song:'I Just Wanna Run',
+        pic:'//y.gtimg.cn/music/photo_new/T002R300x300M000000cXyeS2d5ODS.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004gClXb3cvZtA.m4a?vkey=68919A921EE2D49C51DC1D61C311DA1946C8608D9A1DC086AD247B1B676C1886201D909F4C6FF8C997EEBE5FCD2A0E625D83C55F0864C01C&guid=0&uin=0&fromtag=66',
+        singer:'Maroon 5',
+        song:'Sugar ',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M0000006ftqt2nAMYV.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400002rZws71mnykf.m4a?vkey=2923B15C22270DB6571B842DB662DE546B86B78B67E3CD2D4C15F9D6E161CC3F68B6F17375939581E79269E5D4A7B4C57ED8CFBF065830E7&guid=0&uin=0&fromtag=66',
+        singer:'One Direction',
+        song:'What Makes You Beautiful',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000000cNt5s0HBytP.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400003ZBLM747kvDD.m4a?vkey=CDE1B441BA1D1AE06C1FF16E45B3A6DADACA8334C1460806D1D52B0419863A276B1B467583DDDB1CA0DAA04C2968DA66608813C93C147214&guid=0&uin=0&fromtag=66',
+        singer:'Lovestoned',
+        song:'Bye Bye Bye',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004BoV2s32W6at.jpg?max_age=2592000'
+    },{
+        url:'http://m10.music.126.net/20171020122107/13d78f279dfb6fb80b051eb2582ec34a/ymusic/d981/01c4/8b0d/7cd300b627d2ed3f186dfbe4bd4eb452.mp3',
+        singer:'Darin',
+        song:'B What U Wanna B',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001w7xrX3jbMHh.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400000jF0b70x9gDf.m4a?vkey=0DABC858B3668F0D289689D3DB1EFF0EE43C95DFAEE0801D30BAF3A4BF2B8725B7F82F41BEC407B374EF8269BC811778BE8FF0D02E24FFA0&guid=0&uin=0&fromtag=66',
+        singer:'Alan Walker',
+        song:'Faded',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002Nt51E0q8Zoo.jpg?max_age=2592000'
+    },{
+        url:'http://m10.music.126.net/20171020122932/9ed912d7d25674a4bf417e748cdb1529/ymusic/85ec/a998/a159/ba80574d0e5f01019638b8ebfdcb3ed9.mp3',
+        singer:'Lady Antebellum',
+        song:'Need You Now',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000004TrKJf2WRrP8.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004anWbm2S4qaq.m4a?vkey=B041DA5D5BA65514F7582EE5E5FB7EF05B8345159EFBD465C96B6299CEEBB3D0554DFEAE5D5008BD0C3A20D50E28AB673F6181ECC91C8381&guid=0&uin=0&fromtag=66',
+        singer:'Owl City / Carly Rae Jepsen',
+        song:'Good Time',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001mat8F3tAzsu.jpg?max_age=2592000'
+    },{
+        url:'http://m10.music.126.net/20171020123809/3184bf533a9034423d40869a4ba4f313/ymusic/df42/d10a/fb01/9cf464e75d0513aebfb80c21de44cf02.mp3',
+        singer:'Delacey',
+        song:'Dream It Possible',
+        pic:'https://y.gtimg.cn/music/photo_new/T001R300x300M000003nDphF0lH7M8.jpg?max_age=2592000'
+    },{
+        url:'http://dl.stream.qqmusic.qq.com/C400004MoemE1VV2ft.m4a?vkey=A63420B757CC1938BB366787C19698E56641C69636A65637C6B3B7AC58E579EC8262300951B657970992C84C3541FE479F66A66959CD96EB&guid=0&uin=0&fromtag=66',
+        singer:'Ellie Goulding',
+        song:'Love Me Like You Do',
+        pic:'https://y.gtimg.cn/music/photo_new/T002R300x300M000003pKram2DPwm5.jpg?max_age=2592000'
+    }
+
+
+];
